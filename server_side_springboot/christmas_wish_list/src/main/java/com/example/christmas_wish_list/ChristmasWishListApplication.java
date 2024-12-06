eac5201f3a3e9313d5886f7406671b2654e91177
Ulugbek Yarkinov <42yaul1mst@hft-stuttgart.de>
2024-12-06 18:15:35 +0000

    initial spring boot java project

---
+++ server_side_springboot/christmas_wish_list/src/main/java/com/example/christmas_wish_list/ChristmasWishListApplication.java
@@ -0,0 +1,13 @@
+package com.example.christmas_wish_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasWishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasWishListApplication.class, args);
+	}
+
+}
---
+++ server_side_springboot/christmas_wish_list/src/test/java/com/example/christmas_wish_list/ChristmasWishListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.christmas_wish_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristmasWishListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
